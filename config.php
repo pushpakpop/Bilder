5e2a1507f4bd681a0f060058a57987e604537ccc
Pushpak Patel <pushpak.pop@gmail.com>
2013-06-13 09:46:02 +0530

    config file updated

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
    require 'lib/facebook-php-sdk/facebook.php';
    //Facebook Object
 	$facebook = new Facebook(array(
-	  'appId'  => '570825722948145', //for live app
-	 'secret' => '34d148a644ec071f1da8c347aa6419db', //for live app
+	  'appId'  => '', //Provide your app id here
+	 'secret' => '', // provide your app secret key here
 	  'cookie' => true,
 	));
 	
